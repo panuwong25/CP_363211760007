759150b886ec1e9992ed87b71c8b4b5be4230fa9
amonrat panuwong <amonrat.pa@rmutsvmail.com>
2020-07-14 17:57:46 +0700

    add code 2 lines

---
+++ src/lab1/HolloWorld.java
@@ -0,0 +1,15 @@
+package lab1;
+
+public class HolloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hollo World !!!");
+        System.out.println("amonrat panuwong");
+        System.out.println("Nickname gift");
+        System.out.println("age 21");
+        System.out.println("telphon 0984358537");
+
+    }
+
+
+}
