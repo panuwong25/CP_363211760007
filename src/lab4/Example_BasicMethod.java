e14c90c9bb847d5d69d4178f3541bfb15afebe03
panuwong25 <amonrat.pa@rmusvmail.com>
2020-08-25 18:33:52 +0700

    lab3 : if,if

---
+++ src/lab4/Example_BasicMethod.java
@@ -0,0 +1,34 @@
+package lab4;
+
+public class Example_BasicMethod {
+
+    public static void B(int num){
+      //statement
+        System.out.println("Hello B." +num);
+
+    }//B
+    public static int C(int x,int y) {
+        //statement
+        System.out.println("Hello C.");
+        int sum = x + y;
+        return sum;
+    }//C
+
+    public static void main(String[] args) {
+        System.out.println("Hello Main.");
+        //call A()
+        A();
+        A();
+        A();
+        B(100);
+        int sum = C(100,200);
+        System.out.println(sum);
+        System.out.println(C(500,600));
+    }//main
+    public static void A(){
+        //statement
+        System.out.println("Hollo A.");
+       B(500);
+    }//A
+
+}//class
