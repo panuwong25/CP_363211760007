40a2cae4327704a6a724c0118382cd7bb5f60a62
panuwong25 <amonrat.pa@rmusvmail.com>
2020-08-25 17:09:49 +0700

    lab3 : if,if

--- src/lab3/Example_Do_whiie.java
+++ src/lab3/Example_Do_whiie.java
@@ -1,19 +1,14 @@
 package lab3;
 
 import java.util.Scanner;
-
 public class Example_Do_whiie {
-    public static void main(String[] args) {
-
-
+    public static  void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num;
         do {
-            System.out.println("Enter integer: ");
+            System.out.println("Exnter integer: ");
             num = sc.nextInt();
-        }while (num !=0);
-        System.out.println("End, Good Bye");
-
-
-    }
-}
+        } while (num != 0);
+        System.out.println("End Good Bay");
+    }//main
+}//class
