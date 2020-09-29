6f3f7a9619a4641908d640a4801be7e24bec4dc3
panuwong25 <amonrat.pa@rmusvmail.com>
2020-09-29 14:31:51 +0700

    lab3 : if,if

--- src/lab5/BaiuscArray.java
+++ src/lab5/BaiuscArray.java
@@ -2,85 +2,70 @@ package lab5;
 
 import java.util.Scanner;
 
-public class BaiuscArray {
+    public class BaiuscArray {
+        public static void main (String[] args){
 
-    public static void main (String [] args){
 
-        int [] number;
-        number = new int[5];  // 5 dlocks,index  = 0-4
 
-        //display data in array
-        displayArray(number);
+            int[]number ;
+            number = new int [5]; // blocks,index = 0-4
+            //display data in array
+            displayArray(number);
+            number[0] = 100;
+            number[1] = 200;
+            number[2] = 300;
+            number[3] = 400;
+            number[4] = 500;
+            displayArray(number);
+            //input data to array
+            number = inputArray(number);
+            displayArray(number);
+            //find total value in array
+            totalValueArray(number);
+            //find max value in array
+            findmaxValueArray(number);
+            //find minimun value in array
+            findminimunValueArray(number);
 
-        number [0] = 100;
-        number [1] = 200;
-        number [2] = 300;
-        number [3] = 400;
-        number [4] = 500;
-        displayArray (number);
-
-        //input data to array
-       number = inputArray (number);
-       displayArray(number);
-
-       //find total vaiue in array
-        totalValueArray (number);
-
-       //find max value in array
-        findMaxValueArray (number);
-
-       //find minimum in array
-        findMinValueArray (number);
-
-}//main
-
-    private static void findMinValueArray(int[] number) {
-        int min = number [0];
-        for (int i = 0; i > number.length  ; i++) {
-            if (min <= number [i]){
-                min = number [i];
-
-        }
-    }
-
-    private static void findMaxValueArray(int[] number) {
-        int max = number[0];
-        for (int i = 0; i < number.length ; i++) {
-            if (max <= number [i]){
-                max = number [i];
-
+        }//main
+        private static void findminimunValueArray(int[] number) {
+            int minimun = number[0];
+            for (int i = 0; i >number.length ; i++){
+                if (minimun >= number[i])
+                    minimun = number[i];
             }
+            System.out.println("The minimum value in array );: "+minimun);
         }
-        System.out.println("The maximum value in array:"+max);
-    }
 
-
-    private static void totalValueArray (int[] number){
-        int total = 0;
-        for (int val:number) {
-            total += val;
+        private static void findmaxValueArray(int[] number) {
+            int max = number[0];
+            for (int i = 0; i <number.length ; i++) {
+                if (max <= number[i])
+                    max = number[i];
+            }
+            System.out.println("The max value in array );: "+max);
         }
-       System.out.println("Total vaiue in array:" +total);
-    }
-
-
-    private static int[] inputArray (int[] number) {
-        System.out.println("Please, input data to array: ");
-        Scanner sc = new Scanner(System.in);
-
-        for (int i = 0; i < number.length; i++) {
-            System.out.println("number ["+i+"]:");
-              number [i] = sc.nextInt();
-
+        private static void totalValueArray(int[] number) {
+            int total = 0;
+            for (int val:number){
+                total += val;
+            }
+            System.out.println("Total value in array: "+total);
         }
-       return number;
-    }
-
-    private static void displayArray(int[] number) {
-        System.out.println("Data in array");
-        for (int val:number) {
-            System.out.print(val+ " ");
+        private static int[] inputArray(int[] number) {
+            System.out.println("Please,input data ta array: ");
+            Scanner sc = new Scanner(System.in);
+            for (int i = 0; i < number.length; i++) {
+                System.out.print("number["+i+"]:  ");
+                number[i] = sc.nextInt();
+            }
+            return number;
         }
-        System.out.println();
-    }
-}//class
+        private static void displayArray(int[] number) {
+            System.out.println("Data in array");
+            for (int val:number) {
+                System.out.print(val+"  ");
+            }
+            System.out.println();
+        }
+    }//class
--- src/lab5/BasicArray2D.java
+++ src/lab5/BasicArray2D.java
@@ -3,44 +3,35 @@ package lab5;
 import java.util.Scanner;
 
 public class BasicArray2D {
-public static void main(String[] args) {
+    public static void main (String[] args){
+        int number[][] = new int [2][3]; // 2*3 = 6 blocks
+        //input Data to array 2D
+        number = inputData (number);
+        //display data to  array 2 D
+        displayArray(number);
 
-    int number [][] = new int[2][3]; // 2* 3 = 6 blocks
+    }
+    private static void displayArray(int[][] number) {
+        System.out.println("Data in array 2D:  " );
+        for (int i = 0; i <number.length ; i++) { //row
+            for (int j = 0; j <number[i].length ; j++) { //column
+                System.out.print(number[i][j]+"  ");  //number[0][0]:
 
-         //input Data to array 2D
-        number = inputData (number) ;
-        //displarArray data in array 2D
-        displarArray (number);
-        }
-
-    private static void displarArray(int[][] number) {
-        Scanner sc = new Scanner(System.in) ;
-        System.out .println("Data in array 2D:") ;
-        for (int i = 0; i < number.length ; i++) {//row
-            for (int j = 0; j < number[i].length; j++) { //column
-                System.out.print(number [i][j]+""); //number [0][0];
 
             }
             System.out.println();
         }
     }
-
     private static int[][] inputData(int[][] number) {
-
-        Scanner sc = new Scanner(System.in) ;
-        System.out .println("Please, input integers to array 2D:") ;
-        for (int i = 0; i < number.length ; i++) {  //row
-            for (int j = 0; j < number[i].length; j++) {    //column
-                System.out.print("number["+i+"]["+j+"]: ");    // number [0][0]:
-                number [i][j] = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Pleasa,input integers to array 2D:  ");
+        for (int i = 0; i < number.length; i++) { //row
+            for (int j = 0; j < number[i].length; j++) { //column
+                System.out.print("number[" + i + "][" + j + "]:");//number[0][0]:
+                number[i][j] = sc.nextInt();
 
             }
-
         }
-        return number;
-
-
+        return  number;
     }
-
-
-}class
+}
